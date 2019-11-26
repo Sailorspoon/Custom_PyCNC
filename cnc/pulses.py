c317d49411e184c90be5eaf1eaa5f43dd934aaa9
Julian Olbrich <julian.olbrich@freenet.de>
2019-11-26 09:57:57 +0100

    Delta Logik vervollständigt

--- cnc/pulses.py
+++ cnc/pulses.py
@@ -7,7 +7,6 @@ from cnc.enums import *
 
 SECONDS_IN_MINUTE = 60.0
 
-
 class PulseGenerator(object):
     """ Stepper motors pulses generator.
         It generates time for each pulses for specified path as accelerated
@@ -256,22 +255,33 @@ class PulseGeneratorLinear(PulseGenerator):
         super(PulseGeneratorLinear, self).__init__(delta_mm)
         #distance_mm = abs(delta_mm)  # type: Coordinates
         # Berechnung der Carriage Höhen abhängig von der aktuellen Tool Position
-        distance_pivot_carriage_mm.a = math.sqrt((radius_heatbed - (tx + distance_pivot_tool_mm)) ** + (0 - ty) ** 2)
-        height_carriage_mm.a = tz + height_pivot_tool_mm + math.sqrt(length_arm.a ** 2
+        height_carriage_mm_old.a = height_carriage_mm.a
+        height_carriage_mm_old.b = height_carriage_mm.b
+        height_carriage_mm_old.c = height_carriage_mm.c
+
+        tu = dict()
+        tu.x = Coordinates.x
+        tu.y = Coordinates.y
+        tu.z = Coordinates.z
+
+        distance_pivot_carriage_mm.a = math.sqrt((radius_heatbed - (tu.x + distance_pivot_tool_mm)) ** + (0 - ty) ** 2)
+        height_carriage_mm.a = tu.z + height_pivot_tool_mm + math.sqrt(length_arm.a ** 2
                                                                      - distance_pivot_carriage_mm.a ** 2)
         distance_pivot_carriage_mm.b = math.sqrt((radius_heatbed * math.cos(120)
                                                   - (tx + distance_pivot_tool_mm * math.cos(120))) ** 2
-                                                 + (radius_heatbed * math.sin(120) - (ty + distance_pivot_tool_mm
+                                                 + (radius_heatbed * math.sin(120) - (tu.y + distance_pivot_tool_mm
                                                                                       * math.sin(120))) ** 2)
-        height_carriage_mm.b = tz + height_pivot_tool_mm + math.sqrt(length_arm.b ** 2
+        height_carriage_mm.b = tu.z + height_pivot_tool_mm + math.sqrt(length_arm.b ** 2
                                                                      - distance_pivot_carriage_mm.b ** 2)
         distance_pivot_carriage_mm.c = math.sqrt((radius_heatbed * math.cos(240)
-                                                  - (tx + distance_pivot_tool_mm * math.cos(240))) ** 2
-                                                 + (radius_heatbed * math.sin(240) - (ty + distance_pivot_tool_mm
+                                                  - (tu.x + distance_pivot_tool_mm * math.cos(240))) ** 2
+                                                 + (radius_heatbed * math.sin(240) - (tu.y + distance_pivot_tool_mm
                                                                                       * math.sin(240))) ** 2)
-        height_carriage_mm.c = tz + height_pivot_tool_mm + math.sqrt(length_arm.c ** 2
+        height_carriage_mm.c = tu.z + height_pivot_tool_mm + math.sqrt(length_arm.c ** 2
                                                                      - distance_pivot_carriage_mm.c ** 2)
-
+        distance_mm.x = height_carriage_mm.a - height_carriage_mm_old.a
+        distance_mm.y = height_carriage_mm.b - height_carriage_mm_old.b
+        distance_mm.z = height_carriage_mm.c - height_carriage_mm_old.c
         # velocity of each axis
         distance_total_mm = distance_mm.length()
         self.max_velocity_mm_per_sec = self._adjust_velocity(distance_mm * (
